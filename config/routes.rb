bf2937ba1031bbae9d477ce9b449eb1b46ec4aba
Ulysse Schwartz <schwartzulysse@gmail.com>
2017-02-09 12:50:35 +0100

    heroku launching 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:index]
+  root to: 'cocktails#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+file = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+json = JSON.parse(file.read.to_s)
+json["drinks"].each do |hash|
+  Ingredient.create(name: hash["strIngredient1"])
+end
